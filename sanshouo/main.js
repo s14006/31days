5db48c4713ba65416f60969aeb2c00e2d2c7d93a
s14006 <s14006@std.it-college.ac.jp>
2015-07-06 19:28:17 +0900

    2015/07/06

--- sanshouo/main.js
+++ sanshouo/main.js
@@ -2,12 +2,40 @@ enchant();
 
 var gs = {fps:15, height:320, width:320};
 
+var ga = {};
+ga.oo3 = {
+	height:32
+	,image:"oo3.png"
+	,width:47
+};
+
+var eSprite = enchant.Class.create(enchant.Sprite,{
+	initialize:function(asset){
+		enchant.Sprite.call(this,asset.width,asset.height);
+		this.image = game.assets[asset.image];
+		game.currentScene.addChild(this);
+	}
+});
+
+var Oo3 = enchant.Class.create(eSprite,{
+	initialize:function(frame,x,y) {
+		eSprite.call(this,ga.oo3);
+		this.frame = frame || 0;
+		this.x = x || 0;
+		this.y = y || 0;
+	}
+});
+
+
 window.onload = function() {
 	game = new Core(gs.width, gs.height);
 	game.fps = gs.fps;
+	game.preload(ga.oo3.image);
 	stage = game.rootScene;
 
 	game.onload = function() {
+
+		var oo3 = new Oo3([0,1,2],100,100);
 		
 	};
 
