0db27312a11af2bea442988f37671c130ccbaac4
s14006 <s14006@std.it-college.ac.jp>
2015-07-09 12:38:04 +0900

    2015/07/09

--- sanshouo/main.js
+++ sanshouo/main.js
@@ -3,20 +3,29 @@ enchant();
 var gs = {fps:30, height:320, width:320};
 
 var ga = {};
-ga.oo3 = {
-	height:32
-	,image:"oo3.png"
-	,width:47
+
+ga.background = {
+	height:320
+	,image:"background.png"
+	,width:320
 };
+
+ga.dai = {
+	height:80
+	,image:"dai.png"
+	,width:50
+};
+
 ga.kassha = {
 	height:100
 	,image:"kassha.png"
 	,width:100
 };
-ga.dai = {
-	height:80
-	,image:"dai.png"
-	,width:50
+
+ga.oo3 = {
+	height:32
+	,image:"oo3.png"
+	,width:47
 };
 
 var eSprite = enchant.Class.create(enchant.Sprite,{
@@ -31,16 +40,16 @@ var Oo3 = enchant.Class.create(eSprite,{
 	initialize:function() {
 		eSprite.call(this,ga.oo3);
 		this.frame = [0, 0, 1, 1, 2, 2];
-		this.x = 120;
-		this.y = 220;
+		this.x = 70;
+		this.y = 120;
 	}
 });
 
 var Kassha = enchant.Class.create(eSprite,{
 	initialize:function() {
 		eSprite.call(this,ga.kassha);
-		this.x = 95;
-		this.y = 157;
+		this.x = 45;
+		this.y = 57;
 	}
 
 	,onenterframe:function() {
@@ -51,8 +60,8 @@ var Kassha = enchant.Class.create(eSprite,{
 var Dai = enchant.Class.create(eSprite,{
 	initialize:function() {
 		eSprite.call(this,ga.dai);
-		this.x = 120;
-		this.y = 190;
+		this.x = 70;
+		this.y = 90;
 	}
 });
 
@@ -77,10 +86,12 @@ var Player = enchant.Class.create(Oo3,{
 window.onload = function() {
 	game = new Core(gs.width, gs.height);
 	game.fps = gs.fps;
-	game.preload(ga.oo3.image,ga.kassha.image,ga.dai.image);
+	game.preload(ga.background.image,ga.oo3.image,ga.kassha.image,ga.dai.image);
 	stage = game.rootScene;
 
 	game.onload = function() {
+
+		var background = new eSprite(ga.background);
 
 		var dai = new Dai();
 		var kassha = new Kassha();
