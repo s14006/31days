e9bc6df740c2987b1af6bae24de12dbc388ba3b6
s14006 <s14006@std.it-college.ac.jp>
2015-07-09 23:10:51 +0900

    2015/07/09

--- sanshouo/main.js
+++ sanshouo/main.js
@@ -8,7 +8,7 @@ ga.background = {height:320, image:"background.png", width:320};
 
 ga.oo3 = {height:32, image:"oo3.png", width:47};
 
-ga.pulley = {height:100, image:"pulley.png", width:100};
+ga.pulleyBody = {height:100, image:"pulley.png", width:100};
 
 ga.stand = {height:80, image:"stand.png", width:50};
 
@@ -21,19 +21,19 @@ var eSprite = enchant.Class.create(enchant.Sprite, {
 });
 
 var Oo3 = enchant.Class.create(eSprite,{
-	initialize:function() {
+	initialize:function(x, y) {
 		eSprite.call(this, ga.oo3);
 		this.frame = [0, 0, 1, 1, 2, 2];
-		this.x = 70;
-		this.y = 120;
+		this.x = 0 || x;
+		this.y = 0 || y;
 	}
 });
 
-var Pulley = enchant.Class.create(eSprite, {
-	initialize:function() {
-		eSprite.call(this, ga.pulley);
-		this.x = 45;
-		this.y = 57;
+var PulleyBody = enchant.Class.create(eSprite, {
+	initialize:function(x, y) {
+		eSprite.call(this, ga.pulleyBody);
+		this.x = 0 || x;
+		this.y = 0 || y;
 	}
 
 	,onenterframe:function() {
@@ -41,12 +41,21 @@ var Pulley = enchant.Class.create(eSprite, {
 	}
 });
 
-
-var Stand = enchant.Class.create(eSprite,{
-	initialize:function() {
+var PulleyStand = enchant.Class.create(eSprite,{
+	initialize:function(x, y) {
 		eSprite.call(this,ga.stand);
-		this.x = 70;
-		this.y = 90;
+		this.x = 25 + x;
+		this.y = 40 + y;
+	}
+});
+
+var Pulley = enchant.Class.create({
+	initialize:function(x, y) {
+		var group = new Group();
+		group.addChild(new PulleyStand(x, y));
+		group.addChild(new PulleyBody(x, y));
+
+		stage.addChild(group);
 	}
 });
 
@@ -72,16 +81,16 @@ var Player = enchant.Class.create(Oo3,{
 window.onload = function() {
 	game = new Core(gs.width, gs.height);
 	game.fps = gs.fps;
-	game.preload(ga.background.image, ga.oo3.image, ga.pulley.image, ga.stand.image);
+	game.preload(ga.background.image, ga.oo3.image, ga.pulleyBody.image, ga.stand.image);
 	stage = game.rootScene;
 
 	game.onload = function() {
 
-		var background = new eSprite(ga.background);
+		var backgrund = new eSprite(ga.background);
 
-		var stand = new Stand();
-		var pulley = new Pulley();
-		var oo3 = new Oo3();
+		var pulley = new Pulley(43, 56);
+
+		var oo3 = new Oo3(70, 120);
 
 	};
 
